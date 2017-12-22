af1cf7ff29e29d4e7df2395f51616b1844ee4d24
Sam <gebharsj@gmatc.matc.edu>
2017-12-22 11:33:09 -0600

    updated main menu to use rewired, update players based in input instead of ui buttons

--- Assets/Game/Scripts/Utility/AssignControls.cs
+++ Assets/Game/Scripts/Utility/AssignControls.cs
@@ -1,11 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Rewired;
+using TMPro;
 
 public class AssignControls : MonoBehaviour {
 
     bool keyboardAssigned;
+    bool checkForInput;
+
+    Menu menu;
+
+    Button campaignButton;
+    Button cBackButton;
+    TextMeshProUGUI cAddPlayersText;
+
+    Button endlessButton;
+    Button eBackButton;
+    TextMeshProUGUI eAddPlayersText;
+
+    [HideInInspector]
+    public List<bool> controlsAssigned;
+
+    void Awake()
+    {
+        menu = GameObject.Find("Manager").GetComponent<Menu>();
+        SetButtons();
+    }
+
+    void OnLevelWasLoaded(int index)
+    {
+        if(index == 0)
+        {
+            Menu menu = GameObject.Find("Manager").GetComponent<Menu>();
+            SetButtons();
+        }
+    }
+
+    void SetButtons()
+    {
+        campaignButton = menu.menuVariableHandler.campaignButton;
+        campaignButton.onClick.AddListener(ToggleCheckForInput);
+        cBackButton = menu.menuVariableHandler.cBackButton;
+        cBackButton.onClick.AddListener(ToggleCheckForInput);
+        cAddPlayersText = menu.menuVariableHandler.cAddPlayersText;
+
+        endlessButton = menu.menuVariableHandler.endlessButton;
+        endlessButton.onClick.AddListener(ToggleCheckForInput);
+        eBackButton = menu.menuVariableHandler.eBackButton;
+        eBackButton.onClick.AddListener(ToggleCheckForInput);
+        eAddPlayersText = menu.menuVariableHandler.eAddPlayersText;
+    }
+
+    void ToggleCheckForInput()
+    {
+        checkForInput = !checkForInput;
+    }
 
 	void Update () {
         if (!ReInput.isReady) return;
@@ -14,50 +66,58 @@ public class AssignControls : MonoBehaviour {
 
     void AssignControl()
     {
-        if(!keyboardAssigned)
+        if (controlsAssigned.Count < 1 || checkForInput)
         {
-            if(ReInput.controllers.Keyboard.GetAnyButtonDown())
+            if (!keyboardAssigned)
             {
-                Player player = FindPlayerWithoutJoystick();
-                if (player == null) return;
-
-                for(int i = 0; i < ReInput.players.Players.Count; i++)
+                if (ReInput.controllers.Keyboard.GetAnyButtonDown() || ReInput.controllers.Mouse.GetAnyButtonDown())
                 {
-                    ReInput.players.Players[i].controllers.hasKeyboard = false;
-                    ReInput.players.Players[i].controllers.hasMouse = false;
+                    Player player = FindPlayerWithoutJoystick();
+                    if (player == null) return;
+
+                    for (int i = 0; i < ReInput.players.Players.Count; i++)
+                    {
+                        ReInput.players.Players[i].controllers.hasKeyboard = false;
+                        ReInput.players.Players[i].controllers.hasMouse = false;
+                    }
+
+                    player.controllers.hasKeyboard = true;
+                    player.controllers.hasMouse = true;
+                    player.controllers.excludeFromControllerAutoAssignment = true;
+
+                    controlsAssigned.Add(true);
+                    UpdatePlayerCount();
+
+                    keyboardAssigned = true;
                 }
-
-                player.controllers.hasKeyboard = true;
-                player.controllers.hasMouse = true;
-                player.controllers.excludeFromControllerAutoAssignment = true;
-
-                keyboardAssigned = true;
             }
-        }
 
-        IList<Joystick> joysticks = ReInput.controllers.Joysticks;
-        for (int i = 0; i < joysticks.Count; i++)
-        {
-            Joystick joystick = joysticks[i];
-            if (ReInput.controllers.IsControllerAssigned(joystick.type, joystick.id)) continue; // joystick is already assigned to a Player
-
-            // Chec if a button was pressed on the joystick
-            if (joystick.GetAnyButtonDown())
+            IList<Joystick> joysticks = ReInput.controllers.Joysticks;
+            for (int i = 0; i < joysticks.Count; i++)
             {
+                Joystick joystick = joysticks[i];
+                if (ReInput.controllers.IsControllerAssigned(joystick.type, joystick.id)) continue; // joystick is already assigned to a Player
 
-                // Find the next Player without a Joystick
-                Player player = FindPlayerWithoutJoystick();
-                if (player == null) return; // no free joysticks
+                // Chec if a button was pressed on the joystick
+                if (joystick.GetAnyButtonDown())
+                {
+                    // Find the next Player without a Joystick
+                    Player player = FindPlayerWithoutJoystick();
+                    if (player == null) return; // no free joysticks
 
-                // Assign the joystick to this Player
-                player.controllers.AddController(joystick, false);
+                    // Assign the joystick to this Player
+                    player.controllers.AddController(joystick, false);
+
+                    controlsAssigned.Add(true);
+                    UpdatePlayerCount();
+                }
             }
-        }
 
-        if (DoAllPlayersHaveJoysticks())
-        {
-            ReInput.configuration.autoAssignJoysticks = true;
-            this.enabled = false; // disable this script
+            if (DoAllPlayersHaveJoysticks())
+            {
+                ReInput.configuration.autoAssignJoysticks = true;
+                this.enabled = false; // disable this script
+            }
         }
     }
 
@@ -76,5 +136,30 @@ public class AssignControls : MonoBehaviour {
     private bool DoAllPlayersHaveJoysticks()
     {
         return FindPlayerWithoutJoystick() == null;
+    }
+
+    void UpdatePlayerCount()
+    {
+        cAddPlayersText.text = "Player " + (controlsAssigned.Count + 1) + " press any button to join.";
+        eAddPlayersText.text = "Player " + (controlsAssigned.Count + 1) + " press any button to join.";
+
+        if (menu == null) return;
+        switch (controlsAssigned.Count)
+        {
+            case 2:
+                menu.menuVariableHandler.twoPlayers.ChangeCameraPosition();
+                menu.menuVariableHandler.twoPlayers.ChangeCameraRotation();
+                break;
+            case 3:
+                menu.menuVariableHandler.threePlayers.ChangeCameraPosition();
+                menu.menuVariableHandler.threePlayers.ChangeCameraRotation();
+                break;
+            case 4:
+                menu.menuVariableHandler.fourPlayers.ChangeCameraPosition();
+                menu.menuVariableHandler.fourPlayers.ChangeCameraRotation();
+                break;
+            default:
+                break;
+        }
     }
 }
--- Assets/Game/Scripts/Utility/Menu.cs
+++ Assets/Game/Scripts/Utility/Menu.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
+    public MenuVariableHandler menuVariableHandler;
+
     public void LoadScene(int index)
     {
         SceneManager.LoadScene(index);
     }
+}
+
+[System.Serializable]
+public class MenuVariableHandler
+{
+    public Button campaignButton;
+    public Button cBackButton;
+    public TextMeshProUGUI cAddPlayersText;
+    public Button endlessButton;
+    public Button eBackButton;
+    public TextMeshProUGUI eAddPlayersText;
+
+    public ChangeCameraValues twoPlayers;
+    public ChangeCameraValues threePlayers;
+    public ChangeCameraValues fourPlayers;
 }
